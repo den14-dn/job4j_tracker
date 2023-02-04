a739f3fa19ff77136b24de964d571412a807c4f2
Denis <den14-dn@yandex.ru>
2023-02-04 12:24:31 +0300

    12.2. Тесты вывода на консоль [#33585 #4863]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -70,4 +70,86 @@ public class StartUITest {
                 + "0. Exit" + System.lineSeparator()
         );
     }
+
+    @Test
+    public void whenFindAllItems() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Item two = tracker.add(new Item("test2"));
+        Input in = new StubInput(
+                new String[] {"0", "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new ShowAllAction(out),
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        String expect = "Menu:" + ln
+                + "0. Show all items" + ln
+                + "1. Exit" + ln
+                + "=== Show all items ===" + ln
+                + one + ln
+                + two + ln
+                + "Menu:" + ln
+                + "0. Show all items" + ln
+                + "1. Exit" + ln;
+        assertThat(out.toString()).isEqualTo(expect);
+    }
+
+    @Test
+    public void whenFindByNameItems() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test"));
+        Item two = tracker.add(new Item("test"));
+        tracker.add(new Item("test0"));
+        Input in = new StubInput(
+                new String[] {"0", "test", "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new FindByNameAction(out),
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        String expect = "Menu:" + ln
+                + "0. Find by name" + ln
+                + "1. Exit" + ln
+                + "=== Find items by name ===" + ln
+                + one + ln
+                + two + ln
+                + "Menu:" + ln
+                + "0. Find by name" + ln
+                + "1. Exit" + ln;
+        assertThat(out.toString()).isEqualTo(expect);
+    }
+
+    @Test
+    public void whenFindByIdItem() {
+        Output out = new StubOutput();
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("test"));
+        tracker.add(new Item("test"));
+        Item three = tracker.add(new Item("test0"));
+        Input in = new StubInput(
+                new String[] {"0", String.valueOf(three.getId()), "1"}
+        );
+        UserAction[] actions = new UserAction[]{
+                new FindByIdAction(out),
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        String expect = "Menu:" + ln
+                + "0. Find by id" + ln
+                + "1. Exit" + ln
+                + "=== Find item by id ===" + ln
+                + three + ln
+                + "Menu:" + ln
+                + "0. Find by id" + ln
+                + "1. Exit" + ln;
+        assertThat(out.toString()).isEqualTo(expect);
+    }
 }

2b381829d07bd6cbfd32361f6a61b64cd3c87bf5
Denis <den14-dn@yandex.ru>
2023-02-05 00:49:45 +0300

    2. Шаблон фабричный метод [#95067 #388131]

---
+++ src/main/java/ru/job4j/factoryMethod/Shape.java
@@ -0,0 +1,6 @@
+package ru.job4j.factoryMethod;
+
+public interface Shape {
+    String draw();
+    double square();
+}
---
+++ src/main/java/ru/job4j/factoryMethod/Triangle.java
@@ -0,0 +1,25 @@
+package ru.job4j.factoryMethod;
+
+public class Triangle implements Shape {
+    private final int a;
+    private final int h;
+
+    public Triangle(int a, int h) {
+        this.a = a;
+        this.h = h;
+    }
+
+    @Override
+    public String draw() {
+        String ln = System.lineSeparator();
+        return "   *" + ln
+                + "  * *"+ ln
+                + " *   *" + ln
+                + "*******";
+    }
+
+    @Override
+    public double square() {
+        return 0.5 * a * h;
+    }
+}
